6d6cd3e1f5cc12eeba8ec68b8c218b9aa685db99
e.gharib <e.gharib@efarda.ir>
2025-07-09 10:21:16 +0330

    add files

---
+++ packages/mobile-ui/src/mobile-button.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Pressable, Text } from "react-native";
+
+export const MobileButton = ({ children, className, appName }: any) => {
+  return (
+    <Pressable>
+        <Text>Hello</Text>
+    </Pressable>
+  );
+};
